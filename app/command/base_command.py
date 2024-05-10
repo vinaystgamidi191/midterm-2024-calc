779c321d0360d428cb9deafbb3d19b16c9848f46
Your Name <you@example.com>
2024-05-10 05:50:33 +0300

    removed

--- app/command/base_command.py
+++
@@ -1,6 +0,0 @@
-# app/command/base_command.py
-from app.calculation_history import CalculationHistory
-
-class BaseCommand:
-    def __init__(self):
-        self.history_instance = CalculationHistory()
